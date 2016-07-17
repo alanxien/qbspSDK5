b8970ab7fd8696b1b74d16cbd5cd412213c1f042
alanxien <624558339@qq.com>
2016-07-17 19:46:57 +0800

    111

--- src/com/chuannuo/tangguo/imageLoader/ImageSizeUtil.java
+++ src/com/chuannuo/tangguo/imageLoader/ImageSizeUtil.java
@@ -57,37 +57,43 @@ public class ImageSizeUtil
 			DisplayMetrics displayMetrics = imageView.getContext().getResources()
 					.getDisplayMetrics();
 			LayoutParams lp = imageView.getLayoutParams();
+			
+			if(lp==null){
+				imageSize.width = 600;
+				imageSize.height = 600;
+			}else{
+				int width = imageView.getWidth();// 获取imageview的实际宽度
+				if (width <= 0)
+				{
+					width = lp.width;// 获取imageview在layout中声明的宽度
+				}
+				if (width <= 0)
+				{
+					 //width = imageView.getMaxWidth();// 检查最大值
+					width = getImageViewFieldValue(imageView, "mMaxWidth");
+				}
+				if (width <= 0)
+				{
+					width = displayMetrics.widthPixels;
+				}
 
-			int width = imageView.getWidth();// 获取imageview的实际宽度
-			if (width <= 0)
-			{
-				width = lp.width;// 获取imageview在layout中声明的宽度
+				int height = imageView.getHeight();// 获取imageview的实际高度
+				if (height <= 0)
+				{
+					height = lp.height;// 获取imageview在layout中声明的宽度
+				}
+				if (height <= 0)
+				{
+					height = getImageViewFieldValue(imageView, "mMaxHeight");// 检查最大值
+				}
+				if (height <= 0)
+				{
+					height = displayMetrics.heightPixels;
+				}
+				imageSize.width = width;
+				imageSize.height = height;
 			}
-			if (width <= 0)
-			{
-				 //width = imageView.getMaxWidth();// 检查最大值
-				width = getImageViewFieldValue(imageView, "mMaxWidth");
-			}
-			if (width <= 0)
-			{
-				width = displayMetrics.widthPixels;
-			}
-
-			int height = imageView.getHeight();// 获取imageview的实际高度
-			if (height <= 0)
-			{
-				height = lp.height;// 获取imageview在layout中声明的宽度
-			}
-			if (height <= 0)
-			{
-				height = getImageViewFieldValue(imageView, "mMaxHeight");// 检查最大值
-			}
-			if (height <= 0)
-			{
-				height = displayMetrics.heightPixels;
-			}
-			imageSize.width = width;
-			imageSize.height = height;
+			
 		}
 
 		return imageSize;
